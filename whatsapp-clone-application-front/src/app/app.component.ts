f664f3ed5cadd513ed9dfefa6cb131bae6123c42
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2024-07-04 11:24:56 +0100

    Task 3: add fontawesome dependency to the front app

--- whatsapp-clone-application-front/src/app/app.component.ts
+++ whatsapp-clone-application-front/src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {NgbAccordionModule} from "@ng-bootstrap/ng-bootstrap";
+import {FaIconComponent, FaIconLibrary} from "@fortawesome/angular-fontawesome";
+import {fontAwesomeIcons} from "./shared/font-awesome-icons";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NgbAccordionModule],
+  imports: [RouterOutlet, NgbAccordionModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'whatsapp-clone-application-front';
+
+  private faIconLibrary = inject(FaIconLibrary);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
